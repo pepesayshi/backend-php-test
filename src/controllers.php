e76c3f6c0a10c2656cd03c3bf3ad0bfe34dd1fba
pepesayshi <sean.li.lxy@gmail.com>
2022-04-25 13:43:31 +1200

    Task-extra - prepare statement for sql injection prevention

--- src/controllers.php
+++ src/controllers.php
@@ -5,7 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
-
     return $twig;
 }));
 
@@ -18,17 +17,27 @@ $app->get('/', function () use ($app) {
 
 
 $app->match('/login', function (Request $request) use ($app) {
+
     $username = $request->get('username');
     $password = $request->get('password');
 
     if ($username) {
-        $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-        $user = $app['db']->fetchAssoc($sql);
+
+        // 1. only select the columns needed for query performance (dont save password in the session)
+        // 2. prepare statement to prevent sql injection & fetch record
+        // 3. hashed password was stored in db for security reasons
+        $user = $app['db']->fetchAssoc("
+            SELECT `id`, `username`
+            FROM `users` 
+            WHERE `username` = ? 
+            AND `password` = ?
+        ", [$username, md5($password)]);
 
         if ($user){
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
         }
+
     }
 
     return $app['twig']->render('login.html', array());
@@ -42,20 +51,33 @@ $app->get('/logout', function () use ($app) {
 
 
 $app->get('/todo/{id}', function ($id) use ($app) {
+
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
 
     if ($id){
-        $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $app['db']->fetchAssoc($sql);
+
+        // prepare statement to prevent sql injection & fetch record
+        $todo = $app['db']->fetchAssoc("
+            SELECT *
+            FROM `todos`
+            WHERE `id` = ?
+        ", [$id]);
 
         return $app['twig']->render('todo.html', [
             'todo' => $todo,
         ]);
+
     } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
+
+        // 1. prepare statement to prevent sql injection & fetch record
+        // 2. handle exception - if $user['id'] is undefined for some reason, see no post
+        $todos = $app['db']->fetchAll("
+            SELECT *
+            FROM `todos`
+            WHERE `user_id` = ?
+        ", [$user['id'] ?? null]);
 
         return $app['twig']->render('todos.html', [
             'todos' => $todos,
@@ -66,15 +88,21 @@ $app->get('/todo/{id}', function ($id) use ($app) {
 
 
 $app->post('/todo/add', function (Request $request) use ($app) {
+
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
 
-    $user_id = $user['id'];
+    // in case its undefined
+    $user_id = $user['id'] ?? null;
     $description = $request->get('description');
 
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+    // prepare statement to insert
+    $app['db']->executeUpdate("
+        INSERT INTO `todos`
+        (`user_id`, `description`) 
+        VALUES (?, ?)
+    ", [$user_id, $description]);
 
     return $app->redirect('/todo');
 });
@@ -82,8 +110,11 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
 
-    $sql = "DELETE FROM todos WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    // prepare statement to prevent sql injection
+    $affectedrows = $app['db']->executeUpdate("
+        DELETE FROM `todos`
+        WHERE `id` = ?
+    ", [$id]);
 
     return $app->redirect('/todo');
 });
